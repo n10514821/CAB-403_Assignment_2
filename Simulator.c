ed4e33abb220ee0c74f41b099d2c8d8dbc0ca192
Ayesha-ahmad01 <114333842+Ayesha-ahmad01@users.noreply.github.com>
2022-09-25 22:36:46 +1000

    Update Simulator.c
    This is not the best way to do what I have done but it is a starter.

--- Simulator.c
+++ Simulator.c
@@ -2,9 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-//generates 50 random licence plate numbers
+#include <string.h>
+//generates random licence plate numbers everytime it is run
 #define LENGTH_LICENCEPLATE 5
+
+//delaying time 
+void delay_ms(int seconds)
+{
+    int milli_sec = 1000 *seconds;
+
+    clock_t start = clock();
+    while(clock() < start + milli_sec);
+}
+
+
 int main (void) 
 {
     srand((time(NULL)));
@@ -13,17 +24,19 @@ int main (void)
 
     for(int i = 0; i <= 50; i++)
     {
+        delay_ms(0.1); // generates a plate number every 100ms
         for(int i = 0; i <= LENGTH_LICENCEPLATE; i ++)
         {
+            char number = num[rand() % (sizeof num - 1)];
+            char alphabet = alpha[rand() % (sizeof alpha - 1)];
             if(i <= 2)
             {
-                printf("%c", num[rand() % (sizeof num - 1)]);
+                printf("%c", number);
             }
             else
             {
-                printf("%c", alpha[rand() % (sizeof alpha - 1)]);
+                printf("%c",alphabet);
             }
-            
         }
         printf("\n");
 
