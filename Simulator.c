29c5dd62d0ce55e34e449c550e416c383ccc312d
Ayesha-ahmad01 <114333842+Ayesha-ahmad01@users.noreply.github.com>
2022-09-25 22:41:22 +1000

    Update Simulator.c

--- Simulator.c
+++ Simulator.c
@@ -6,8 +6,7 @@
 //generates random licence plate numbers everytime it is run
 #define LENGTH_LICENCEPLATE 5
 
-//delaying time 
-void delay_ms(int seconds)
+time_t delay_ms(int seconds)
 {
     int milli_sec = 1000 *seconds;
 
@@ -24,7 +23,7 @@ int main (void)
 
     for(int i = 0; i <= 50; i++)
     {
-        delay_ms(0.1); // generates a plate number every 100ms
+        rand() % delay_ms(0.1); // generate between 1- 100ms
         for(int i = 0; i <= LENGTH_LICENCEPLATE; i ++)
         {
             char number = num[rand() % (sizeof num - 1)];
