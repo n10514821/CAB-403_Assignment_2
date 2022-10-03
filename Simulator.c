91ab56b21b79b202e79705ed056191e5bece6961
Ayesha-ahmad01 <114333842+Ayesha-ahmad01@users.noreply.github.com>
2022-10-03 22:38:41 +1000

    Update Simulator.c

--- Simulator.c
+++ Simulator.c
@@ -3,8 +3,74 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-//generates random licence plate numbers everytime it is run
+#include <semaphore.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+// deifnes
+#define PARKING_SIZE 2920
 #define LENGTH_LICENCEPLATE 5
+
+
+// create the shared parking (not sure what should go in here)
+typedef struct shared_carpark
+{
+    sem_t Fire_alarm_semaphore;
+    sem_t Manager_sempahore;
+    sem_t Simulator_semaphore;
+
+}shared_carpark_t;
+
+// shared memory structure
+typedef struct shared_memory
+{
+    const char *name;
+
+    int fd;
+
+    //address of shared carpark block
+    shared_carpark_t *data;
+
+}shared_memory_t;
+
+// PARKING struct
+typedef struct PARKING
+{
+    struct Exit;
+    struct Level;
+    struct Entrance;
+} shared_carpark_t;
+
+//shared memory file descriptor and pointer to shared memory object
+int shm_fd;
+void *ptr;
+
+bool create_shared_object(shared_memory_t *shm, const char *share_name)
+{
+    shm_unlink(share_name);
+
+    //insert share name to shm->name
+    shm->name = share_name;
+
+    //create shared memory object
+    shm_fd = shm_open(share_name,O_CREAT|O_RDWR,0666);
+    shm->fd = shm_fd; 
+
+    //configure the size of the shared memory object
+    ftruncate(shm_fd,PARKING_SIZE);
+
+    ptr = mmap(0,PARKING_SIZE, MAP_SHARED, shm_fd,0);
+    shm->data = ptr;
+
+    return true;
+}
+//generates random licence plate numbers everytime it is run
 
 time_t delay_ms(int seconds)
 {
