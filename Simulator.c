e463bb14b8f68ea45aff03f842ef045487a67607
Ayesha-ahmad01 <114333842+Ayesha-ahmad01@users.noreply.github.com>
2022-10-05 14:24:09 +1000

    Update Simulator.c

--- Simulator.c
+++ Simulator.c
@@ -12,10 +12,25 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <pthread.h>
 
-// deifnes
+
+// DEFINES
 #define PARKING_SIZE 2920
+//generates random licence plate numbers everytime it is run
+#define LENGTH_LICENCEPLATE 5
 
+// GLOBALS
+// create a file pointer for communucation between file a program
+FILE *fileptr;
+//create a mutex lock
+pthread_mutex_t lock;
+//shared memory file descriptor and pointer to shared memory object
+int shm_fd;
+void *ptr;
+
+
+//STRUCTS
 // create the shared parking (not sure what should go in here)
 typedef struct shared_carpark
 {
@@ -45,9 +60,8 @@ typedef struct PARKING
     struct Entrance;
 } shared_carpark_t;
 
-//shared memory file descriptor and pointer to shared memory object
-int shm_fd;
-void *ptr;
+
+//FUNCTIONS
 
 bool create_shared_object(shared_memory_t *shm, const char *share_name)
 {
@@ -69,16 +83,7 @@ bool create_shared_object(shared_memory_t *shm, const char *share_name)
     return true;
 }
 
-
-
-
-//generates random licence plate numbers everytime it is run
-#define LENGTH_LICENCEPLATE 5
-
-// globals
-    // create a file pointer for communucation between file a program
-    FILE *fileptr;
-
+//time delay
 time_t delay_ms(int seconds)
 {
     int milli_sec = 1000 *seconds;
@@ -87,8 +92,10 @@ time_t delay_ms(int seconds)
     while(clock() < start + milli_sec);
 }
 
+//generate plate numbers
 void generate_plate_number()
 {
+    pthread_mutex_lock(&lock);
     char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char num[] = "0123456789"; 
     for(int i = 0; i <= LENGTH_LICENCEPLATE; i ++)
@@ -105,12 +112,15 @@ void generate_plate_number()
         }       
     }
     printf("\n");
+    pthread_mutex_unlock(&lock);
+
    
 }
 
+//reads a file.
 void read_file()
 {
-
+    pthread_mutex_lock(&lock);
     char plate_num[7];
     //open file 
     fileptr = fopen("plates.txt","r +");
@@ -129,10 +139,12 @@ void read_file()
         printf("Error in opening file");
         exit(1);
     }
+    pthread_mutex_unlock(&lock);
 }
 
 void generate_mix_plates()
 {
+    pthread_mutex_lock(&lock);
     for (int i = 0; i <= rand()%delay_ms(0.1); i++)
     {
         if(i <= rand()%1)
@@ -142,10 +154,10 @@ void generate_mix_plates()
         else
         {
             generate_plate_number(); 
-        }
-
-    
+        } 
     }
+    pthread_mutex_unlock(&lock);
+
     
 }
 
