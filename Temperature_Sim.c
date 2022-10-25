407ed9ace06fbd3c55dd0ed8b7e66f37d78f36dc
n10514821 <102886683+n10514821@users.noreply.github.com>
2022-10-26 04:12:51 +1000

    Draft
    Currently having issues with a buffer overload and starting shared memory

--- Temperature_Sim.c
+++ Temperature_Sim.c
@@ -5,224 +5,325 @@
 #include <pthread.h> 
 #include <stdbool.h>
 #include <math.h>
+#include <sys/shm.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+//complied with -lrt
+//gcc Temperature_Sim.c -o Temperature_Sim -lrt
 
 
 //temperature and fire alram variables
 #define Temperature_collecting_time 2 //in milliseconds
-#define Temperature_storage_list_inital_capacity  5 //alter this to form a list (keep 5)
-#define Smoothed_Temperatures_list_inital_capacity 30 //alter this to form a list (keep 30)
-#define DV_GROWTH_FACTOR 2
+#define Temperature_storage_list_capacity  5 //This is the max amount of values the list is ment to hold 
+#define Smoothed_Temperatures_list_capacity 30 //This is the max amount of values the list is ment to hold 
 
-#define Temperature_storage_limit  5
-#define Smoothed_Temperatures_limit 30
 
+#define fire_alarm_trigger 0 //if changed, fire alarm is triggered
 
 //Include header files
-#include "Temperature_storage_list.h"
-#include "Smoothed_Temperatures_storage_list.h"
 
+#include "Temperature_header.h"
 
-//This code initals a list to store the randomly generated temperature values
-void TS_init( Temperature_storage_list_t* vec ) {
-	vec->TS_capacity = Temperature_storage_list_inital_capacity;
-	vec->TS_size = 0;
-	
-    vec->temperature_storage = malloc(100);
-  
-}
+//This code creates the two list which hold the collected temperature
+// and the  calculated temperature 
+void List_creation( Combined_Temperature_list_t* vec ) {
 
-//This code pushs a randomly generated value into the first poistion of the list
-void TS_push( Temperature_storage_list_t* vec, int16_t new_temp) {
-    //sets variables 
-    int16_t* old_data = vec->temperature_storage;
-    size_t new_size = vec->TS_size +1; 
-    size_t old_size = vec->TS_size; 
+   vec->Collected_Temperature_capacity= Temperature_storage_list_capacity;
+   vec->Collected_Temperature_size=0;
+   vec->Collected_Temperature_storage= malloc(0);
 
-//This checks if the size of list is over 5
-//if so it will remove the last value from the list 
-    if (new_size>Temperature_storage_limit)
-    {
-       for(int i = old_size;i>=1;i--){
-    vec->temperature_storage[i] = old_data[i-1];
-     }
-    vec->temperature_storage[0] = new_temp;
-     }
-    //This code is used to determine if it is first value to be added to the list
-     else if(new_size == 1){
-        vec->TS_size = new_size;
-        vec->temperature_storage[0] = new_temp;
-     }
-     //This code is used there is already one value in the list and does not 
-     //already contain five values 
-    else{
-    vec->TS_size = new_size;
-     for(int i = old_size;i>=0;i--){
-    vec->temperature_storage[i] = old_data[i-1];
-     }
-    vec->temperature_storage[0] = new_temp;
-    }
-}
-
-//This prints all values sotrd in the list
-void print_all_temps(Temperature_storage_list_t* vec){
-size_t size = vec->TS_size;
-for(int i=0;i<size;i++){
-    printf("%d \n",vec->temperature_storage[i]);
-}
+   vec->Smoothe_Temperature_capacity= Smoothed_Temperatures_list_capacity;
+   vec->Smoothe_Temperature_size=0;
+   vec->Smoothe_Temperature_storage=malloc(0);
+   
 }
 
 
-//This code initals a list to store the calulated Smoothed_Temperatures
-void ST_init( Smoothed_Temperatures_storage_list_t* vec ) {
-    vec->ST_capacity = Smoothed_Temperatures_list_inital_capacity;
-	vec->ST_size =0 ;
-    vec->Smoothed_Temperatures_storage = malloc(100);
-}
-
-//Create and push Smooth Temperature
-//input values are the vectors for ST and TS
-void Calulate_and_Push_ST(Smoothed_Temperatures_storage_list_t* vec_1,Temperature_storage_list_t* vec_2){
-//retrive and store data from each vector
-int16_t* ST_storage = vec_1->Smoothed_Temperatures_storage;
-int16_t* TS_storage= vec_2->temperature_storage;
-
-size_t ST_storage_size = vec_1->ST_size;
-size_t TS_storage_size= vec_2->TS_size;
-
-//create average TS values
-int16_t Smooth_Temp_value =0; //set inital value;
-for(int i=0; i<Temperature_storage_limit;i++){
-   Smooth_Temp_value=TS_storage[i]+Smooth_Temp_value;
-}
-//creates average value. Total values / number of values (the limit)
-Smooth_Temp_value=Smooth_Temp_value/Temperature_storage_limit; 
-
-if(vec_1->ST_size=0){
-    printf("tes1t\n");
-vec_1->Smoothed_Temperatures_storage[0] = Smooth_Temp_value;
-vec_1->ST_size=1;
-}
-else if(ST_storage_size >=Smoothed_Temperatures_limit){
-    printf("tes13t\n");
- for(int i = TS_storage_size;i>=1;i--){
-    vec_1->Smoothed_Temperatures_storage[i] = ST_storage[i-1];
-     }
-    vec_1->Smoothed_Temperatures_storage[0] = Smooth_Temp_value;
-    vec_1->ST_size= ST_storage_size +1;
-}
-else{
-    printf("tes1t2\n");
-    vec_1->ST_size = ST_storage_size+1;
-     for(int i = TS_storage_size;i>=0;i--){
-    vec_1->Smoothed_Temperatures_storage[i] = ST_storage[i-1];
-     }
-    vec_1->Smoothed_Temperatures_storage[0] = Smooth_Temp_value;
-    
-    }
-}
-
-
-//This prints all values sotrd in the list
-void print_all_STtemps(Smoothed_Temperatures_storage_list_t* vec){
-size_t size = vec->ST_size;
-printf("%d this is size \n",size);
-for(int i=0;i<size;i++){
-    
-    printf("%d \n",vec->Smoothed_Temperatures_storage[i]);
-}
-}
-
-
-//generates a random temperature
+//This code is used to generate a random temperature 
 int16_t generate_temperature(){
- // srand(time(0)); 
-  int16_t random_temperture =rand() % 65; //does not say which 
+ 
+  int16_t random_temperture =rand() % 46+20; //does not say which 
   //temps are allowed but states it is stored in int16_t. used its value
   printf("%d generated number \n",random_temperture);
   return random_temperture;
 }
 
+//This code is used to place code into lists
+void Temperature_System (Combined_Temperature_list_t* vec, int16_t Temperature ){
+//List variables
+ //size_t Current_collected_temperature_length = vec->Collected_Temperature_size;
+
+ size_t New_collected_temperature_length = vec->Collected_Temperature_size +1;
+ printf("This is the New_collected_temperature_length %d \n",New_collected_temperature_length);
+ int16_t* Current_collected_temperature_data = vec->Collected_Temperature_storage;
 
 
-//checking for fire dectection 1 (fixed temp)
-int fixed_temp_alarm(Smoothed_Temperatures_storage_list_t* vec){
-int16_t* data = vec->Smoothed_Temperatures_storage;
-int holder =0;
-for(int i=0; i<Smoothed_Temperatures_limit;i++){
-     if(data[i] >= 58){
-      holder= holder+1;
-     }
-     if(holder>=Smoothed_Temperatures_limit *0.9){
-        //trigger fire alarm
-        return 1;
-     }
-     return 0;
+// This section of code will focus on adding the collected temperature to
+//the list
+
+//This section is when the array begins
+if(New_collected_temperature_length ==1){
+    printf("s %d \n",New_collected_temperature_length);
+vec->Collected_Temperature_storage[0]=Temperature;
+vec->Collected_Temperature_size=New_collected_temperature_length;
+printf("printing array\n");
+printf("%d \n",vec->Collected_Temperature_storage[0]);
+ printf("\n");
+}
+
+
+//This section is when the array is at its max length
+else if(New_collected_temperature_length>=Temperature_storage_list_capacity){
+    New_collected_temperature_length=Temperature_storage_list_capacity;
+printf("E %d \n" ,New_collected_temperature_length);
+for(size_t i=New_collected_temperature_length;i>=1;i-- ){
+     
+vec->Collected_Temperature_storage[i]=Current_collected_temperature_data[i-1];
+
+}
+vec->Collected_Temperature_storage[0]=Temperature;
+
+vec->Collected_Temperature_size=Temperature_storage_list_capacity;
+printf("printing array\n");
+for (size_t i = 0; i <= New_collected_temperature_length -1; i++)
+{ 
+    printf("%d \n",vec->Collected_Temperature_storage[i]);
+}
+
+ printf("\n");
+}
+
+
+//This section is when the array is not at its max length and has
+//a vaule in it.
+else {
+    printf("Middle %d \n",New_collected_temperature_length);
+    for(size_t i=New_collected_temperature_length;i>=1;i-- ){
+    vec->Collected_Temperature_storage[i]=Current_collected_temperature_data[i-1];
+    
+}
+vec->Collected_Temperature_storage[0]=Temperature;
+vec->Collected_Temperature_size=New_collected_temperature_length;
+ printf("printing array\n");
+for (size_t i = 0; i <= New_collected_temperature_length -1; i++)
+{ 
+    printf("%d \n",vec->Collected_Temperature_storage[i]);
+}
+ printf("\n");
+}
+
+
+
+
+
+
+//This section now focues on creating the smooth temperature and placing
+//in a List 
+if (vec->Collected_Temperature_size >= Temperature_storage_list_capacity){
+   
+//varaibles
+
+size_t Current_smoothe_temperature_length = vec->Smoothe_Temperature_size;
+
+size_t New_smoothe_temperature_length =vec->Smoothe_Temperature_size+1;
+int16_t* Current_smoothe_temperature_data = vec->Smoothe_Temperature_storage;
+
+
+//calculate the smooth temperature
+int16_t sum_of_CT_array =0;
+for(int i=0;i<Temperature_storage_list_capacity ;i++){
+ sum_of_CT_array=sum_of_CT_array+ Current_collected_temperature_data[i];
+}
+int16_t average_of_CT_array=sum_of_CT_array/Temperature_storage_list_capacity;
+//This section of code is used to place the smooth temp in the array
+printf("ST value is %d \n",average_of_CT_array);
+printf("\n");
+
+if (New_smoothe_temperature_length ==1){
+    printf("Start of ST %d \n",Current_smoothe_temperature_length);
+     vec->Smoothe_Temperature_storage[0]=average_of_CT_array;
+     printf("%d is ST \n", vec->Smoothe_Temperature_storage[0]);
+     vec->Smoothe_Temperature_size = New_smoothe_temperature_length;
+     printf("\n");
+}
+
+
+
+else if(New_smoothe_temperature_length<Smoothed_Temperatures_list_capacity) {
+    printf("Middle %d \n",New_smoothe_temperature_length);
+    for(size_t i=New_smoothe_temperature_length;i>=1;i-- ){
+    vec->Smoothe_Temperature_storage[i]=Current_smoothe_temperature_data[i-1];
+    
+}
+vec->Smoothe_Temperature_storage[0]=average_of_CT_array;
+vec->Smoothe_Temperature_size=New_smoothe_temperature_length;
+ printf("printing array\n");
+for (size_t i = 0; i <= New_smoothe_temperature_length -1; i++)
+{ 
+    printf("%d \n",vec->Smoothe_Temperature_storage[i]);
+}
+ printf("\n");
+}
+
+else if(New_smoothe_temperature_length >= Smoothed_Temperatures_list_capacity){ 
+     printf("End of ST %d \n",New_smoothe_temperature_length);
+New_smoothe_temperature_length=Smoothed_Temperatures_list_capacity;
+
+for(int i=New_smoothe_temperature_length; i>=1;i--){
+vec->Smoothe_Temperature_storage[i]=Current_smoothe_temperature_data[i-1];
+}
+vec->Smoothe_Temperature_storage[0]=average_of_CT_array;
+vec->Smoothe_Temperature_size = New_smoothe_temperature_length;
+for (size_t i = 0; i <= New_smoothe_temperature_length -1; i++)
+{ 
+    printf("%d \n",vec->Smoothe_Temperature_storage[i]);
+}
+ printf("\n");
+}
+
+}
+
+}
+
+
+
+
+
+
+
+/*
+void shared_memory(){
+    const char *Level_one_fire_sensor_char = "Level_one_fire_sensor";
+    const int Fire_sensor_level_one_byte_size =2;
+    int Level_one_fire_sensor= shm_open(Level_one_fire_sensor_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_one_fire_sensor, Fire_sensor_level_one_byte_size);
+    shm_unlink(Level_one_fire_sensor_char); 
+
+    const char *Level_one_fire_alarm_char = "Level_one_fire_alarm";
+    const int Fire_alarm_level_one_byte_size =1;
+    int Level_one_fire_alarm= shm_open(Level_one_fire_alarm_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_one_fire_alarm, Fire_alarm_level_one_byte_size);
+    shm_unlink(Level_one_fire_alarm_char);
+
+
+    const char *Level_two_fire_sensor_char = "Level_two_fire_sensor";
+    const int Fire_sensor_level_two_byte_size =2;
+    int Level_two_fire_sensor= shm_open(Level_two_fire_sensor_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_two_fire_sensor, Fire_sensor_level_two_byte_size);
+    shm_unlink(Level_two_fire_sensor_char);
+
+    const char *Level_two_fire_alarm_char = "Level_two_fire_alarm";
+    const int Fire_alarm_level_two_byte_size =2;
+    int Level_two_fire_alam= shm_open(Level_two_fire_alarm_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_two_fire_alam, Fire_alarm_level_two_byte_size);
+    shm_unlink(Level_two_fire_alarm_char);
+
+
+    const char *Level_three_fire_sensor_char = "Level_three_fire_sensor";
+    const int Fire_sensor_level_three_byte_size =2;
+    int Level_three_fire_sensor= shm_open(Level_three_fire_sensor_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_three_fire_sensor, Fire_sensor_level_three_byte_size);
+    shm_unlink(Level_three_fire_sensor_char);
+
+    const char *Level_three_fire_alarm_char = "Level_three_fire_alarm";
+    const int Fire_alarm_level_three_byte_size =2;
+    int Level_three_fire_alarm= shm_open(Level_three_fire_sensor_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_three_fire_alarm, Fire_alarm_level_three_byte_size);
+    shm_unlink(Level_three_fire_alarm_char);
+
+
+    const char *Level_four_fire_sensor_char = "Level_four_fire_sensor";
+    const int Fire_sensor_level_four_byte_size =2;
+    int Level_four_fire_sensor= shm_open(Level_four_fire_sensor_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_four_fire_sensor, Fire_sensor_level_four_byte_size);
+    shm_unlink(Level_four_fire_sensor_char);
+
+    const char *Level_four_fire_alarm_char = "Level_four_fire_alarm";
+    const int Fire_alarm_level_four_byte_size =1;
+    int Level_four_fire_alarm= shm_open(Level_four_fire_alarm_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_four_fire_alarm, Fire_alarm_level_four_byte_size);
+    shm_unlink(Level_four_fire_alarm_char);
+
+
+    const char *Level_five_fire_sensor_char = "Level_five_fire_sensor";
+    const int Fire_sensor_level_5_byte_size =2;
+    int Level_five_fire_sensor= shm_open(Level_five_fire_sensor_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_five_fire_sensor, Fire_sensor_level_5_byte_size);
+    shm_unlink(Level_five_fire_sensor_char);
+ 
+
+    const char *Level_five_fire_alarm_char = "Level_five_fire_alarm";
+    const int Fire_alarm_level_5_byte_size =1;
+    int Level_five_fire_alarm= shm_open(Level_five_fire_alarm_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_five_fire_alarm, Fire_alarm_level_5_byte_size);
+    shm_unlink(Level_five_fire_alarm_char);
+    
+}
+
+*/
+void print_all_Recored_temps(Combined_Temperature_list_t* vec){
+size_t size = vec->Collected_Temperature_size;
+for(int i=0;i<size;i++){
+    printf("%d is recored temp\n",vec->Collected_Temperature_storage[i]);
+}
+}
+void print_all_Smooth_temps(Combined_Temperature_list_t* vec){
+size_t size = vec->Smoothe_Temperature_size;
+for(int i=0;i<size;i++){
+    printf("%d is smooth temp\n",vec->Smoothe_Temperature_storage[i]);
 }
 }
 
 
-//checking for fire dectection 2 (rate of rise)
-int Rate_Of_Rise_alarm(Smoothed_Temperatures_storage_list_t* vec){
-int16_t* data = vec->Smoothed_Temperatures_storage; 
-if(data[Smoothed_Temperatures_limit-1]-data[0] >=8 ){
-    return 1;
-}
-return 0;
 
-}
+
 
 
 int main(int argc, char const *argv[])
 {
-	Temperature_storage_list_t vector;
-	TS_init(&vector);
-
-    Smoothed_Temperatures_storage_list_t vector_2;
-    ST_init(&vector_2);
 
 
-	TS_push(&vector,generate_temperature());
-    printf("1st printed numbers\n");
-    print_all_temps(&vector);
-    printf("\n");
+//we want to create a 2 byte long variable for level 1 which contains the ability 
+Combined_Temperature_list_t Level_1_Temperature;
+List_creation(&Level_1_Temperature);
+/*
 
-    TS_push(&vector,generate_temperature());
-    printf("2nd printed numbers n");
-    print_all_temps(&vector);
-    printf("\n");
-
-    TS_push(&vector,generate_temperature());
-    printf("3rd printed numbers\n");
-    print_all_temps(&vector);
-    printf("\n");
-
-    TS_push(&vector,generate_temperature());
-    printf("4th printed numbers\n");
-    print_all_temps(&vector);
-    printf("\n");
-    
-    TS_push(&vector,generate_temperature());
-    printf("5th printed numbers\n");
-    print_all_temps(&vector);
-    printf("\n");
-
-   printf("calulate ST\n");
-   Calulate_and_Push_ST(&vector_2,&vector);
-   print_all_STtemps(&vector_2);
-   printf("\n");
-   
+ const char *Test = "Test";
+    const int Fire_sensor_level_one_byte_size =2;
+    int Level_one_fire_sensor= shm_open(Level_one_fire_sensor_char, O_CREAT|O_RDWR , 0666);
+    ftruncate(Level_one_fire_sensor, Fire_sensor_level_one_byte_size);
+    size_t test =2 ;
+   void *mmap(NULL,test, PROT_READ | PROT_WRITE, MAP_SHARED, test, 0);
 
 
-    TS_push(&vector,generate_temperature());
-    printf("6th printed numbers\n");
-    print_all_temps(&vector);
-    printf("\n");
 
-     printf("calulate ST\n");
-   Calulate_and_Push_ST(&vector_2,&vector);
-   print_all_STtemps(&vector_2);
-   printf("\n");
+*/
 
 
+int starter =1;
+while (starter <36)
+{
+int16_t Temperature_record= generate_temperature();
+
+Temperature_System(&Level_1_Temperature,Temperature_record);
+//print_all_Recored_temps(&Level_1_Temperature);
+
+if(starter >=6){
+    //print_all_Smooth_temps(&Level_1_Temperature);
+}
+
+starter=starter+1;
   
+}
+
+
+
 }
