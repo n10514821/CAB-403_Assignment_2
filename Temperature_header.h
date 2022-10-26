ddc7312cc2992f9cd674533955a6bd9020c9b207
n10514821 <102886683+n10514821@users.noreply.github.com>
2022-10-26 13:58:00 +1000

    Add files via upload

---
+++ Temperature_header.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//create list for storing all temperature values
+#include <stdbool.h>
+#include <math.h>
+typedef struct Combined_Temperature_list{
+    //Collected_Temperature
+    size_t Collected_Temperature_size;
+    //Capacity
+    size_t Collected_Temperature_capacity;
+	//Store data
+	int16_t* Collected_Temperature_storage;
+
+    //Calulated Temperater
+	//size
+	size_t Smoothe_Temperature_size;
+	//Capacity
+	size_t Smoothe_Temperature_capacity;
+	//Store data
+	int16_t* Smoothe_Temperature_storage;
+
+} Combined_Temperature_list_t;
