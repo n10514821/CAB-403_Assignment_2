0dffb53b7e2fe91e45dc35fcba8d0e424a3e8c8c
n10514821 <102886683+n10514821@users.noreply.github.com>
2022-10-30 18:23:26 +1000

    Updated

--- GUI.c
+++ GUI.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
@@ -13,6 +13,27 @@
 
 
 
+//The following chars are the used to create the table headers
+char  Table_Header_Entry_Boom_gate[]= {'|','E','n','t','r','y',' ','G','a','t','e'};
+char  Table_Header_Exit_Boom_gate[]= {'|','E','x','i','t',' ','G','a','t','e'};
+char  Table_Header_Entry_LPR[]= {'|','E','n','t','r','y',' ','L','P','R'};
+char  Table_Header_Exit_LPR[]= {'|','E','x','i','t',' ','L','P','R'};
+char  Table_Header_Digital_sign[]= {'|','D','i','g','i','t',' ','S','i','g','n'};
+char  Table_Header_Number_of_cars[]= {'|','N','u','m','b','e','r',' ','O','f',' ','C','a','r','s'};
+char  Table_Header_Car_Capacity[]= {'|','C','a','r',' ','C','a','p','a','c','i','t','y'};
+char  Table_Header_State_of_fire_alarm[]= {'|','F','I','R','E',' ','A','L','A','R','M','|'};
+char  Table_Header_status[]={'|',' ',' ','S','t','a','t','u','s',' ',' ','|' };
+char  Table_Header_status_2[]={'|',' ','S','t','a','t','u','s',' ',' ','|' };
+char  Table_Header_status_3[]={'|',' ','S','t','a','t','u','s',' ','|' };
+char  Table_Header_Present_1[]={'|',' ',' ','i','n',' ','c','a','r','p','a','r','k',' ',' ','|'};
+char  Table_Header_Present_2[]={'|',' ','o','f',' ','c','a','r','p','a','r','k',' ','|'};
+char  Table_Header_Digital_sign_expression[]= {'|',' ','D','i','s','p','l','a','y',' ',' ','|'};
+char  Table_Header_Temperature[] = {'T','e','m','p','e','r','a','t','u','r','e','|'};
+char  Table_Header_Temperature_2[] = {'(','D',')','C','e','l','s','i','u','s',' ','|'};
+char Total_revenue[] ={'T','O','T','A','L',' ','R','E','V','E','N','U','E',':','$'};
+
+char  Table_Header_NP_reader[] ={'|',' ',' ','P','l','a','t','e',' ',' ','|' };;
+char  Table_Header_NP_reader_2[]={'|',' ','P','l','a','t','e',' ',' ','|' };
 
 
 char GUI[Height][Width];
@@ -41,7 +62,7 @@ for(x=0;x<Width;x++){
 
 
 
-void GUI_Insert_info(){
+void GUI_Create_table(){
 
 //Title 
 char  Title[] ={'C','A','R',' ','P','A','R','K',' ','S','O','F','T','W','A','R','E'};
@@ -51,32 +72,9 @@ for(int i=0;i<sizeof(Title);i++){
 }
 
 //Set out table here
-//Note
-//1 entry and 1 exit for boom gate , LPR per level,digital sign for entry only
-//# of cars per level and its capacity
-//Current state of fire alarm 
-//Total revenue 
 
 
-char  Table_Header_Entry_Boom_gate[]= {'|','E','n','t','r','y',' ','G','a','t','e'};
-char  Table_Header_Exit_Boom_gate[]= {'|','E','x','i','t',' ','G','a','t','e'};
-char  Table_Header_Entry_LPR[]= {'|','E','n','t','r','y',' ','L','P','R'};
-char  Table_Header_Exit_LPR[]= {'|','E','x','i','t',' ','L','P','R'};
-char  Table_Header_Digital_sign[]= {'|','D','i','g','i','t',' ','S','i','g','n'};
-char  Table_Header_Number_of_cars[]= {'|','N','u','m','b','e','r',' ','O','f',' ','C','a','r','s'};
-char  Table_Header_Car_Capacity[]= {'|','C','a','r',' ','C','a','p','a','c','i','t','y'};
-char  Table_Header_State_of_fire_alarm[]= {'|','F','I','R','E',' ','A','L','A','R','M','|'};
-char  Table_Header_status[]={'|',' ',' ','S','t','a','t','u','s',' ',' ','|' };
-char  Table_Header_status_2[]={'|',' ','S','t','a','t','u','s',' ',' ','|' };
-char  Table_Header_status_3[]={'|',' ','S','t','a','t','u','s',' ','|' };
-char  Table_Header_Present_1[]={'|',' ',' ','i','n',' ','c','a','r','p','a','r','k',' ',' ','|'};
-char  Table_Header_Present_2[]={'|',' ','o','f',' ','c','a','r','p','a','r','k',' ','|'};
 
-char  Table_Header_Digital_sign_expression[]= {'|',' ','D','i','s','p','l','a','y',' ',' ','|'};
-
-char  Table_Header_Temperature[] = {'T','e','m','p','e','r','a','t','u','r','e','|'};
-char  Table_Header_Temperature_2[] = {'(','D',')','C','e','l','s','i','u','s',' ','|'};
-char Total_revenue[] ={'T','O','T','A','L',' ','R','E','V','E','N','U','E',':','$'};
 
 for(int i=1;i<9+sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity) +sizeof(Table_Header_State_of_fire_alarm)+sizeof(Table_Header_Temperature);i++) {
 //This creates the table headers holders
@@ -84,7 +82,7 @@ if(i>8)
 GUI[2][i] = '_';
 GUI[5][i] = '-';
 }
-//This creates the table headers 
+//This creates the table headers on the terminal 
 for (int i=0;i<sizeof(Table_Header_Present_1);i++){
 
     if(i<sizeof(Table_Header_Entry_Boom_gate))
@@ -95,22 +93,20 @@ for (int i=0;i<sizeof(Table_Header_Present_1);i++){
 
     if (i<sizeof(Table_Header_Exit_Boom_gate))
     GUI[3][i+9 +sizeof(Table_Header_Entry_Boom_gate)] =  Table_Header_Exit_Boom_gate[i];
-    
-
     if(i<sizeof(Table_Header_status_2))
     GUI[4][i+9+sizeof(Table_Header_Entry_Boom_gate)] =  Table_Header_status_2[i];
    
 
     if(i<sizeof(Table_Header_Entry_LPR))
     GUI[3][i+9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)] =  Table_Header_Entry_LPR[i];
-    if(i<sizeof(Table_Header_status_2))
-    GUI[4][i+9+sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)] =  Table_Header_status_2[i];
+    if(i<sizeof(Table_Header_NP_reader))
+    GUI[4][i+9+sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)] =  Table_Header_NP_reader[i];
     
 
     if(i<sizeof(Table_Header_Exit_LPR))
     GUI[3][i+9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)] =  Table_Header_Exit_LPR[i];
-    if(i<sizeof(Table_Header_status_3))
-    GUI[4][i+9+sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)] =  Table_Header_status_3[i];
+    if(i<sizeof(Table_Header_NP_reader_2))
+    GUI[4][i+9+sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)] =  Table_Header_NP_reader_2[i];
 
 
     if(i<sizeof(Table_Header_Digital_sign))
@@ -146,8 +142,8 @@ for (int i=0;i<sizeof(Table_Header_Present_1);i++){
 
 
 
-//
 
+//This creates the level 
 int selected_level=1;
 for(int k= 1; k<=Number_of_levels*2;k++){
 char Print_Level[] = {'L','E','V','E','L',' ',selected_level+'0'}; 
@@ -155,14 +151,14 @@ if( k & 1==1){
     selected_level=selected_level+1; }
 
 
-    else{
+    else{//This is used to create a line under each level
     for(int i=1;i<9+sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity) +sizeof(Table_Header_State_of_fire_alarm)+ sizeof(Table_Header_Temperature);i++) {
     GUI[5+k][i] = '_';
     }
     }
     
 
- //Insert coloumns and data into table 
+ //Insert coloumns into table 
 for(int i=0;i<sizeof(Print_Level);i++){
     if( k & 1==1){
     GUI[5+k][1+i] = Print_Level[i];
@@ -177,74 +173,8 @@ for(int i=0;i<sizeof(Print_Level);i++){
     GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+sizeof(Table_Header_State_of_fire_alarm)-1]  ='|';
     GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+sizeof(Table_Header_State_of_fire_alarm)+ sizeof(Table_Header_Temperature)-1]  ='|';
     
-    
-    //INSERT DATA INTO COLOUMNS 
 
-
-    //entry gate status
-    
-    GUI[5+k][10+4] ='t';
-  
-    //exit gate status
-    
-    GUI[5+k][9+sizeof(Table_Header_Entry_Boom_gate)+5] ='s';
-    
-
-    //entry LPR
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+2] ='a';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+3] ='a';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+4] ='a';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+5] ='a';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+6] ='a';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+7] ='a';
-
-    //exit LPR
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+2] ='x';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+3] ='x';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+4] ='x';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+5] ='x';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+6] ='x';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+7] ='x';
-
-    //digital sign
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+5] ='v';
-
-
-
-    //# of cars
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+6] ='e';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+7] ='e';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+8] ='e';
-
-
-
-    //Capacity of cars
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+5] ='k';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+6] ='k';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+7] ='k';
-
-
-
-    //Fire Alarm
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+5] ='m';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+6] ='m';
- 
-
-
-    //Temperature
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+sizeof(Table_Header_State_of_fire_alarm)+4]  ='h';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+sizeof(Table_Header_State_of_fire_alarm)+5]  ='h';
-    GUI[5+k][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+sizeof(Table_Header_State_of_fire_alarm)+6]  ='h';
-
-
-    //total revenue:
-    GUI[5 +Number_of_levels*2 +2][9+sizeof(Total_revenue)]='t';
-    GUI[5 +Number_of_levels*2 +2][9+sizeof(Total_revenue)+1]='t';
-    GUI[5 +Number_of_levels*2 +2][9+sizeof(Total_revenue)+2]='.';
-    GUI[5 +Number_of_levels*2 +2][9+sizeof(Total_revenue)+3]='t';
-    GUI[5 +Number_of_levels*2 +2][9+sizeof(Total_revenue)+4]='t';
     }
-  
 }
 
 }
@@ -259,6 +189,111 @@ for(int i=0;i<sizeof(Print_Level);i++){
 
 }
  
+//This is for inserting data
+//int level,char entry_gate,char exit_gate,char entry_LPR[],char exit_LPR[], char Digital_sign,int Number_of_cars,int Car_capacity,char Fire_alarm, int16_t Temperature
+void Insert_data_into_table(int level,char entry_gate,char exit_gate,char entry_LPR[],char exit_LPR[], char Digital_sign[],int Number_of_cars,int Car_capacity,char Fire_alarm[], int16_t Temperature ){
+//set level
+
+level = 4+level*2;
+
+//entry gate status
+GUI[level][10+4] =entry_gate;
+
+
+//exit gate status
+GUI[level][9+sizeof(Table_Header_Entry_Boom_gate)+5] =exit_gate;
+
+//entry_LPR
+//The reason for not using a for loop is due to moving the coloumn for some reason
+if(entry_LPR!=NULL){
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+2] =entry_LPR[0];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+3] =entry_LPR[1];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+4] =entry_LPR[2];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+5] =entry_LPR[3];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+6] =entry_LPR[4];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+7] =entry_LPR[5];
+}
+
+//exit LPR
+
+if(exit_LPR!=NULL){
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+2] =exit_LPR[0];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+3] =exit_LPR[1];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+4] =exit_LPR[2];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+5] =exit_LPR[3];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+6] =exit_LPR[4];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+7] =exit_LPR[5];
+}
+
+
+if(Digital_sign!=NULL){
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+5] =Digital_sign[0];
+}
+
+
+//# of cars
+
+int number_of_digits = floor(log10(abs(Number_of_cars))) + 1;
+char Nboc_convert_to_char[number_of_digits];
+sprintf (Nboc_convert_to_char, "%i", Number_of_cars) ; 
+
+for(int i=0;i<number_of_digits;i++){
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+6+i] =Nboc_convert_to_char[i];
+}
+ 
+
+ //capacity of cars
+int number_of_digits_car_capacity = floor(log10(abs(Car_capacity))) + 1;
+char coc_convert_to_char[number_of_digits_car_capacity];
+sprintf (coc_convert_to_char, "%i", Car_capacity) ; 
+
+for(int i=0;i<number_of_digits_car_capacity;i++){
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+5+i] =coc_convert_to_char[i];
+}
+
+//Fire_alarm
+int size_of_entry = sizeof(Fire_alarm);
+if(size_of_entry-1 >1){
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+5] =Fire_alarm[0];
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+6] =Fire_alarm[1];
+}
+else{
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+5] =Fire_alarm[0];
+
+}
+
+
+
+// Temperature
+int number_of_digits_Temp = floor(log10(abs(Temperature))) + 1;
+char Temp_convert_to_char[number_of_digits_Temp];
+sprintf (Temp_convert_to_char, "%i", Temperature) ; 
+for(int i=0;i<number_of_digits_Temp;i++){
+GUI[level][9 +sizeof(Table_Header_Entry_Boom_gate)+sizeof(Table_Header_Exit_Boom_gate)+sizeof(Table_Header_Entry_LPR)+ sizeof(Table_Header_Exit_LPR)+sizeof(Table_Header_Digital_sign)+sizeof(Table_Header_Number_of_cars)+sizeof(Table_Header_Car_Capacity)+sizeof(Table_Header_State_of_fire_alarm)+4+i]  =Temp_convert_to_char[i];
+}
+
+}
+
+
+
+
+
+
+//float revenue
+void Total_revenue_GUI(float revenue){
+
+int number_of_digits_TR = floor(log10(abs(revenue))) + 1;
+char TR_convert_to_char[number_of_digits_TR];
+sprintf (TR_convert_to_char, "%f", revenue) ; 
+
+for(int i=0;i<number_of_digits_TR+3;i++){
+GUI[5 +Number_of_levels*2 +2][9+sizeof(Total_revenue)+i]=TR_convert_to_char[i];
+}
+}
+
+
+
+
 
 
 
@@ -288,11 +323,26 @@ int x,y;
 
 void main(){
     int starter=1;
+//testing purposes
+    int level=2;
+    char entry_gate='A';
+    char exit_gate ='O';
+    char entry_LPR[] = "123ABC";
+    char exit_LPR[]= "198CAT";
+    char Digital_sign[]= "a";
+    int Number_of_cars= 103;
+    int Car_capacity =20;
+    char Fire_alarm[]= "SB";//for Active ,SB for standby, NA for Not available
+    int16_t Temperature= 42;
+    float revenue =12.00;
+
     while (starter=1)
     {
     system("clear");
     GUI_design();
-    GUI_Insert_info();
+    GUI_Create_table();
+    Insert_data_into_table(level,entry_gate,exit_gate,entry_LPR,exit_LPR,Digital_sign,Number_of_cars,Car_capacity,Fire_alarm,Temperature);
+    Total_revenue_GUI(revenue);
     Show_GUI();
     sleep(50);//50 milliseconds
     }
